f57cac07a0347f4a88c148335423bd22cd45409d
pnvnd <peter@datacrunch.ca>
2022-03-01 19:22:23 -0500

    Added another example

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -19,9 +19,18 @@ const logger = winston.createLogger({
   ],
 });
 
+var getData = function () {
+  var data = {
+      'item1': 'https://images.unsplash.com/photo-1563422156298-c778a278f9a5',
+      'item2': 'https://images.unsplash.com/photo-1620173834206-c029bf322dba',
+      'item3': 'https://images.unsplash.com/photo-1602491673980-73aa38de027a'
+  }
+  return data;
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Express', "data": getData() });
 
   // Log a message
   logger.info('Information, working as intended.');
